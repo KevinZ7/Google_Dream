e3465e2c7bfd4057c260267672d006d23ed1a5d0
Jim Lin <Gengar@Jims-MBP.hitronhub.home>
2018-11-20 21:16:57 -0800

    Changed server to just be bubble tea

--- server/server.js
+++ server/server.js
@@ -6,10 +6,27 @@ const settings = require('./settings.json')
 const knexConfig = require('./knexfile.js').development
 const knex = require('knex')(knexConfig);
 
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('WOW GOOGLE DREAM!!!')
+    knex
+    .select("*")
+    .from("markers")
+    .where('name', 'bubble tea')
+    .then((results) => {
+
+      res.json(results)
+  });
 })
 
 app.listen(PORT, () => {
