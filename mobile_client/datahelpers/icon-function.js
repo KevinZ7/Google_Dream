39efe3165365d06e64c69277d461c470d75d4f91
MPriest <miche.priest@gmail.com>
2018-11-22 18:35:43 -0800

    testing one icon image link

---
+++ mobile_client/datahelpers/icon-function.js
@@ -0,0 +1,11 @@
+function generateIconImage (type) {
+  switch (type) {
+    case 'cafe':
+      require(../assets/type_icons/cafe_icon.png)
+      break;
+    default:
+      require(../assets/images/green_marker.png)
+  }
+}
+
+export default generateIconImage
