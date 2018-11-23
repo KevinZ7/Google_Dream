c5db546c9e4592a0a05d4baab69964abbc27e163
Jim Lin <Gengar@Jims-MacBook-Pro.local>
2018-11-22 20:35:08 -0800

    Prepping for pull

--- mobile_client/datahelpers/icon-function.js
+++ mobile_client/datahelpers/icon-function.js
@@ -18,16 +18,16 @@ function generateIconImage (type) {
       break;
     case 'cafe':
       iconLink = require('../assets/type_icons/bank_icon.png')
-      break;   
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/bar_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/beauty_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/book_store_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/bowling_alley_icon.png')
       break;
@@ -41,20 +41,20 @@ function generateIconImage (type) {
       iconLink = require('../assets/type_icons/car_wash_icon.png')
       break;
     case 'cafe':
-      iconLink = require('../assets/type_icons/convenience_store_icon.png')
+      iconLink = require('../assets/type_icons/convenience_store.png')
       break;
     case 'cafe':
       iconLink = require('../assets/type_icons/dentist_icon.png')
-      break;   
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/department_store_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/doctor_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/electrician_icon.png')
-      break;  
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/florist_icon.png')
       break;
@@ -72,16 +72,16 @@ function generateIconImage (type) {
       break;
     case 'cafe':
       iconLink = require('../assets/type_icons/home_goods_store.png')
-      break;   
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/hospital_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/library_icon.png')
-      break; 
+      break;
     case 'cafe':
-      iconLink = require('../assets/type_icons/liquor_store_icon.png')
-      break;  
+      iconLink = require('../assets/type_icons/liquore_store_icon.png')
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/meal_takeaway_icon.png')
       break;
@@ -99,16 +99,16 @@ function generateIconImage (type) {
       break;
     case 'cafe':
       iconLink = require('../assets/type_icons/parking_icon.png')
-      break;   
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/pet_store_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/pharmacy_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/plumber_icon.png')
-      break; 
+      break;
       case 'cafe':
       iconLink = require('../assets/type_icons/post_office_icon.png')
       break;
@@ -126,21 +126,21 @@ function generateIconImage (type) {
       break;
     case 'cafe':
       iconLink = require('../assets/type_icons/subway_station_icon.png')
-      break;   
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/supermarket_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/transit_station_icon.png')
-      break; 
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/veterinary_care_icon.png')
-      break;    
+      break;
     case 'cafe':
       iconLink = require('../assets/type_icons/zoo_icon.png')
-      break; 
+      break;
     default:
-      iconLink = require('../assets/images/florist_icon.png')
+      iconLink = require('../assets/type_icons/florist_icon.png')
   }
   return iconLink
 }
